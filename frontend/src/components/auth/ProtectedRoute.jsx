fb4519da57f88d1bafe24a11ae384860593d26ba
singhpraveen77 <singhpraveen8893@gmail.com>
2026-07-20 23:26:46 +0530

    redirect file main loader issu e

--- frontend/src/components/auth/ProtectedRoute.jsx
+++ frontend/src/components/auth/ProtectedRoute.jsx
@@ -2,8 +2,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { TailSpin } from 'react-loader-spinner'
 import MainLoader from '../loader/MainLoader.jsx'
-// import MainLoader from '../loader/mainLoader'
-// import mainLoader from '../loader/mainLoader'
+
 
 export default function ProtectedRoute() {
   const { user, loading } = useSelector((state) => state.auth)
--- frontend/src/components/auth/RedirectIfAuth.jsx
+++ frontend/src/components/auth/RedirectIfAuth.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import MainLoader from '../loader/mainLoader'
+import MainLoader from '../loader/MainLoader.jsx'
 
 export default function RedirectIfAuth({ children }) {
   const { user, loading } = useSelector((state) => state.auth)
