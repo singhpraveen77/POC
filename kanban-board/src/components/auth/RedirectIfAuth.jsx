524048ebb4fa3a033a70c34924935b8db64ad360
Praveen Singh <Praveen.Singh@Geminisolutions.com>
2026-07-20 13:45:27 +0530

    added changes

--- kanban-board/src/components/auth/RedirectIfAuth.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router-dom'
-import { useAuth } from '../../context/AuthContext'
-
-export default function RedirectIfAuth({ children }) {
-  const { user } = useAuth()
-  return user ? <Navigate to="/" replace /> : children
-}
