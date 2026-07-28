9aed6cc2cc4273e59da4689fb0ee53a5efd6eede
Praveen Singh <155170839+singhpraveen77@users.noreply.github.com>
2026-07-28 12:49:08 +0530

    fixed prod url

--- frontend/src/axios/axiosImageInstance.js
+++ frontend/src/axios/axiosImageInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_local_BASE_URL;
+const BASE_URL = import.meta.env.VITE_prod_BASE_URL;
 
 export const axiosImageInstance = axios.create({
   baseURL: BASE_URL,
