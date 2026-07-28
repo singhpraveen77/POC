9fc4a20131aa5be3e6f04db743ed271735534735
Praveen Singh <155170839+singhpraveen77@users.noreply.github.com>
2026-07-28 12:49:25 +0530

    fixed prod url

--- frontend/src/axios/axiosInstance.js
+++ frontend/src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_local_BASE_URL;
-// const BASE_URL = import.meta.env.VITE_prod_BASE_URL;
+// const BASE_URL = import.meta.env.VITE_local_BASE_URL;
+const BASE_URL = import.meta.env.VITE_prod_BASE_URL;
 console.log("BaseUrl : ",BASE_URL);
 
 const axiosInstance = axios.create({
