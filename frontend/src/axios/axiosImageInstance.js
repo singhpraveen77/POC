e37a95b04477736d785fc48fc74e0cf6c4c2eb52
Praveen Singh <155170839+singhpraveen77@users.noreply.github.com>
2026-08-02 16:08:19 +0530

    Update axiosImageInstance.js

--- frontend/src/axios/axiosImageInstance.js
+++ frontend/src/axios/axiosImageInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_local_BASE_URL;
+// const BASE_URL = import.meta.env.VITE_local_BASE_URL;
 
-// const BASE_URL = import.meta.env.VITE_prod_BASE_URL;
+const BASE_URL = import.meta.env.VITE_prod_BASE_URL;
 
 export const axiosImageInstance = axios.create({
   baseURL: BASE_URL,
